c710018e42b425e1df4e1c52eb8b26b0be799624
dilean99 <81166954+dilean99@users.noreply.github.com>
2021-03-24 09:52:01 -0300

    Update Semana5_Q1.js

--- Semana5_Q1.js
+++ Semana5_Q1.js
@@ -2,9 +2,9 @@ function createList(valor,mensagem) {
   let quant = window.prompt("Digite a quantidade: ");
   let nome = window.prompt("Digite o nome da lista :");
   for (i=0; i<quant;i++){
-  let lista = document.createElement("LI");
-  let textnode = document.createTextNode(nome,i+1);
-  lista.appendChild(textnode);
-  document.getElementById("myList").appendChild(lista);
-};
+    let lista = document.createElement("LI");
+    let textnode = document.createTextNode(nome,i+1);
+    lista.appendChild(textnode);
+    document.getElementById("myList").appendChild(lista);
+  };
 }
